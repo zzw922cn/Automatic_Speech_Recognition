7c9e29db5f4e51197ac677d430d493190def71c2
zzw922cn <1422459603@qq.com>
2016-12-16 17:37:50 +0800

    update
    Former-commit-id: d96c1ad507edf22587ea8a2b9edaf9cdd0691ad6

---
+++ src/models/seq2seq.py
@@ -0,0 +1,18 @@
+#-*- coding:utf-8 -*-
+#!/usr/bin/python
+''' automatic speech recognition implemented in Tensorflow
+author:
+
+      iiiiiiiiiiii            iiiiiiiiiiii         !!!!!!!             !!!!!!    
+      #        ###            #        ###           ###        I#        #:     
+      #      ###              #      I##;             ##;       ##       ##      
+            ###                     ###               !##      ####      #       
+           ###                     ###                 ###    ## ###    #'       
+         !##;                    `##%                   ##;  ##   ###  ##        
+        ###                     ###                     $## `#     ##  #         
+       ###        #            ###        #              ####      ####;         
+     `###        -#           ###        `#               ###       ###          
+     ##############          ##############               `#         #     
+     
+date:2016-11-07
+'''
