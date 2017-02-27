44fbb25fd52bd561dae90ecc4b806752a641cf3e
zzw922cn <1422459603@qq.com>
2017-02-27 18:21:58 +0800

    update

--- src/Trash/ed_test.py
+++ src/Trash/ed_test.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
+'''
+author:
+
+      iiiiiiiiiiii            iiiiiiiiiiii         !!!!!!!             !!!!!!    
+      #        ###            #        ###           ###        I#        #:     
+      #      ###              #      I##;             ##;       ##       ##      
+            ###                     ###               !##      ####      #       
+           ###                     ###                 ###    ## ###    #'       
+         !##;                    `##%                   ##;  ##   ###  ##        
+        ###                     ###                     $## `#     ##  #         
+       ###        #            ###        #              ####      ####;         
+     `###        -#           ###        `#               ###       ###          
+     ##############          ##############               `#         #     
+     
+date:2016-11-09
+'''
 
 import tensorflow as tf
 import numpy as np
-## 改写list_to_sparse_tensor函数使得其变成统一的编码 ##
-# 原始的音素如下面的phn变量所示，但是在测试的时候，为
-# 了提高性能表现，一些音素需要进行合并，具体的合并办法
-# 在截图中，请根据合并方法，使得targetList变成只包含合
-# 并之后的音素，可以进行重新编码，如原来是0...61，现在
-# 可以变成0...47，目的是计算两个字符串之间的编辑距离。
-# 并且需要改写一下get_edit_distance，使得其函数签名变成
-# 如下形式：
-#	get_edit_distance(hyp_arr,truth_arr,mode='train')
-# mode可以取值'train'或'test'，如果是'train'模式，那么
-# 就使用原始的list_to_sparse_tensor函数，如果是'test'模
-# 式，那么就使用你改写后的list_to_sparse_tensor函数，换
-# 句话说，你把list_to_sparse_tensor函数的签名也写成如下
-# 形式：
-#	list_to_sparse_tensor(targetList,mode='train')
-# 这样就可以在内部设置两种模式的转换。
 
 
 phn = ['aa', 'ae', 'ah', 'ao', 'aw', 'ax', 'ax-h',
