76706da02fdc0c32a081d10f515ce26e21033e4c
zzw922cn <1422459603@qq.com>
2017-05-04 22:40:40 +0800

    fix bugs

--- src/run.sh
+++ src/run.sh
@@ -7,9 +7,9 @@ do
     echo "dataset is index $b"
     if [ $loop -eq 1 ]
     then
-        /usr/bin/python train.py --lb=$b
+        /usr/bin/python train.py --lb=$b || break
     else
-        /usr/bin/python train.py --lb=$b --keep=True
+        /usr/bin/python train.py --lb=$b --keep=True ||break
     /usr/bin/python train.py --mode=test
     fi
 done
