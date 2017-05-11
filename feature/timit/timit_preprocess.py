71ad37399c5afbadc4ac4c6afc71033762bd8919
hiteshpaul <hiteshpaul94@gmal.com>
2017-05-11 13:01:22 +0530

    Change indentation to 4 spaces

--- feature/timit/timit_preprocess.py
+++ feature/timit/timit_preprocess.py
@@ -10,7 +10,6 @@ date:2017-4-15
 
 import sys
 sys.path.append('../')
-sys.dont_write_bytecode = True
 
 '''
 Do MFCC over all *.wav files and parse label file Use os.walk to iterate all files in a root directory
@@ -42,88 +41,87 @@ phn = ['aa', 'ae', 'ah', 'ao', 'aw', 'ax', 'ax-h', 'axr', 'ay', 'b', 'bcl', 'ch'
 #phn = ['sil', 'aa', 'ae', 'ah', 'ao', 'aw', 'ax', 'ax-h', 'ay', 'b', 'ch', 'd', 'dh', 'dx', 'eh', 'el', 'en', 'epi', 'er', 'ey', 'f', 'g', 'hh', 'ih', 'ix', 'iy', 'jh', 'k', 'l', 'm', 'n', 'ng', 'ow', 'oy', 'p', 'q', 'r', 's', 'sh', 't', 'th', 'uh', 'uw', 'v', 'w', 'y', 'z', 'zh']
 
 def wav2feature(rootdir, feat_dir, label_dir, win_len=0.02, win_step=0.01, mode='mfcc', level='phn', keywords='train', seq2seq=False, save=False):
-  count = 0
-  for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-      fullFilename = os.path.join(subdir, file)
-      filenameNoSuffix =  os.path.splitext(fullFilename)[0]
-      if file.endswith('.WAV'):
-        rate = None
-        sig = None
-        try:
-          (rate,sig)= wav.read(fullFilename)
-        except ValueError as e:
-          if e.message == "File format 'NIST'... not understood.":
-            sf = Sndfile(fullFilename, 'r')
-            nframes = sf.nframes
-            sig = sf.read_frames(nframes)
-            rate = sf.samplerate
-        feat = calcfeat_delta_delta(sig,rate,win_length=win_len,win_step=win_step,mode=mode)
-        if mode == 'mfcc':
-          feat = preprocessing.scale(feat)
-        feat = np.transpose(feat)
-        print feat.shape
+    count = 0
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            fullFilename = os.path.join(subdir, file)
+            filenameNoSuffix =  os.path.splitext(fullFilename)[0]
+            if file.endswith('.WAV'):
+                rate = None
+                sig = None
+                try:
+                    (rate,sig)= wav.read(fullFilename)
+                except ValueError as e:
+                    if e.message == "File format 'NIST'... not understood.":
+                        sf = Sndfile(fullFilename, 'r')
+                        nframes = sf.nframes
+                        sig = sf.read_frames(nframes)
+                        rate = sf.samplerate
+                feat = calcfeat_delta_delta(sig,rate,win_length=win_len,win_step=win_step,mode=mode)
+                if mode == 'mfcc':
+                    feat = preprocessing.scale(feat)
+                feat = np.transpose(feat)
+                print feat.shape
 
-        if level == 'phn':
-          labelFilename = filenameNoSuffix + '.PHN'
-          phenome = []
-          with open(labelFilename,'r') as f:
-            if seq2seq is True:
-              phenome.append(len(phn)) # <start token>
-            for line in f.read().splitlines():
-              s=line.split(' ')[2]
-              p_index = phn.index(s)
-              phenome.append(p_index)
-            if seq2seq is True:
-              phenome.append(len(phn)+1) # <end token>
-            print phenome
-          phenome = np.array(phenome)
+                if level == 'phn':
+                    labelFilename = filenameNoSuffix + '.PHN'
+                    phenome = []
+                    with open(labelFilename,'r') as f:
+                        if seq2seq is True:
+                            phenome.append(len(phn)) # <start token>
+                        for line in f.read().splitlines():
+                            s=line.split(' ')[2]
+                            p_index = phn.index(s)
+                            phenome.append(p_index)
+                        if seq2seq is True:
+                            phenome.append(len(phn)+1) # <end token>
+                        print phenome
+                    phenome = np.array(phenome)
 
-        elif level == 'cha':
-          labelFilename = filenameNoSuffix + '.WRD'
-          phenome = []
-          sentence = ''
-          with open(labelFilename,'r') as f:
-            for line in f.read().splitlines():
-              s=line.split(' ')[2]
-              sentence += s+' '
-              if seq2seq is True:
-                phenome.append(28)
-              for c in s:
-                if c=="'":
-                  phenome.append(27)
-                else:
-                  phenome.append(ord(c)-96)
-              phenome.append(0)
+                elif level == 'cha':
+                    labelFilename = filenameNoSuffix + '.WRD'
+                    phenome = []
+                    sentence = ''
+                    with open(labelFilename,'r') as f:
+                        for line in f.read().splitlines():
+                            s=line.split(' ')[2]
+                            sentence += s+' '
+                            if seq2seq is True:
+                                phenome.append(28)
+                            for c in s:
+                                if c=="'":
+                                    phenome.append(27)
+                                else:
+                                    phenome.append(ord(c)-96)
+                            phenome.append(0)
 
-            phenome = phenome[:-1]
-            if seq2seq is True:
-              phenome.append(29)
-          print phenome
-          print sentence
+                        phenome = phenome[:-1]
+                        if seq2seq is True:
+                            phenome.append(29)
+                    print phenome
+                    print sentence
 
-        count+=1
-        print 'file index:',count
-        if save:
-          featureFilename = feat_dir + filenameNoSuffix.split('/')[-2]+'-'+filenameNoSuffix.split('/')[-1]+'.npy'
-          np.save(featureFilename,feat)
-          labelFilename = label_dir + filenameNoSuffix.split('/')[-2]+'-'+filenameNoSuffix.split('/')[-1]+'.npy'
-          print labelFilename
-          np.save(labelFilename,phenome)
-
+                count+=1
+                print 'file index:',count
+                if save:
+                    featureFilename = feat_dir + filenameNoSuffix.split('/')[-2]+'-'+filenameNoSuffix.split('/')[-1]+'.npy'
+                    np.save(featureFilename,feat)
+                    labelFilename = label_dir + filenameNoSuffix.split('/')[-2]+'-'+filenameNoSuffix.split('/')[-1]+'.npy'
+                    print labelFilename
+                    np.save(labelFilename,phenome)
 
 
 if __name__ == '__main__':
-  # character or phoneme
-  level = 'cha'
-  # train or test dataset
-  keywords = 'train'
-  mode = 'mfcc'
-  feat_dir = os.path.join('/home/pony/github/data/timit/', level, keywords, mode)
-  label_dir = os.path.join('/home/pony/github/data/timit/', level, keywords, 'label')
-  if not os.path.exists(label_dir):
-    os.makedirs(label_dir)
-  if not os.path.exists(feat_dir):
-    os.makedirs(feat_dir)
-  rootdir = os.path.join('/media/pony/DLdigest/study/ASR/corpus/TIMIT', keywords)
-  wav2feature(rootdir, feat_dir, label_dir, win_len=0.02, win_step=0.01, mode=mode, level='cha', keywords='train', seq2seq=True, save=False)
+    # character or phoneme
+    level = 'cha'
+    # train or test dataset
+    keywords = 'train'
+    mode = 'mfcc'
+    feat_dir = os.path.join('/home/pony/github/data/timit/', level, keywords, mode)
+    label_dir = os.path.join('/home/pony/github/data/timit/', level, keywords, 'label')
+    if not os.path.exists(label_dir):
+        os.makedirs(label_dir)
+    if not os.path.exists(feat_dir):
+        os.makedirs(feat_dir)
+    rootdir = os.path.join('/media/pony/DLdigest/study/ASR/corpus/TIMIT', keywords)
+    wav2feature(rootdir, feat_dir, label_dir, win_len=0.02, win_step=0.01, mode=mode, level='cha', keywords='train', seq2seq=True, save=False)
