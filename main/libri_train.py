e3930880325b264a8760192d6313a605bb403cd9
zzw922cn <1422459603@qq.com>
2017-05-14 19:27:45 +0800

    finish libri_train.py

--- main/libri_train.py
+++ main/libri_train.py
@@ -48,8 +48,12 @@ from tensorflow.python.platform import flags
 from tensorflow.python.platform import app
     
 flags.DEFINE_string('task', 'libri', 'set task name of this program')
-flags.DEFINE_string('dataset', 'train-clean-100', 'set task name of this program')
-flags.DEFINE_string('mode', 'train', 'set whether to train or test')
+flags.DEFINE_string('train_dataset', 'train-clean-100', 'set the training dataset')
+flags.DEFINE_string('dev_dataset', 'dev-clean', 'set the development dataset')
+flags.DEFINE_string('test_dataset', 'test-clean', 'set the test dataset')
+
+flags.DEFINE_string('mode', 'train', 'set whether to train, dev or test')
+
 flags.DEFINE_boolean('keep', False, 'set whether to restore a model, when test mode, keep should be set to True')
 flags.DEFINE_string('level', 'cha', 'set the task level, phn, cha, or seq2seq, seq2seq will be supported soon')
 flags.DEFINE_string('model', 'DBiRNN', 'set the model to use, DBiRNN, BiRNN, ResNet..')
@@ -60,7 +64,7 @@ flags.DEFINE_string('optimizer', 'adam', 'set the optimizer to use, sgd, adam...
 
 flags.DEFINE_integer('batch_size', 32, 'set the batch size')
 flags.DEFINE_integer('num_hidden', 256, 'set the hidden size of rnn cell')
-flags.DEFINE_integer('num_feature', 39, 'set the size of input feature')
+flags.DEFINE_integer('num_feature', 60, 'set the size of input feature')
 flags.DEFINE_integer('num_classes', 30, 'set the number of output classes')
 flags.DEFINE_integer('num_epochs', 500, 'set the number of epochs')
 flags.DEFINE_float('lr', 0.0001, 'set the learning rate')
@@ -72,7 +76,11 @@ flags.DEFINE_string('logdir', '/home/pony/github/log/libri', 'set the log direct
 
 FLAGS = flags.FLAGS
 task = FLAGS.task
-dataset = FLAGS.dataset
+
+train_dataset = FLAGS.train_dataset
+dev_dataset = FLAGS.dev_dataset
+test_dataset = FLAGS.test_dataset
+
 level = FLAGS.level
 model_fn = model_functions_dict[FLAGS.model]
 rnncell = FLAGS.rnncell
@@ -105,10 +113,32 @@ if mode == 'test':
   batch_size = 100
   num_epochs = 1
 
-train_mfcc_dir = os.path.join(datadir, level, dataset, 'feature')
-train_label_dir = os.path.join(datadir, level, dataset, 'label')
-test_mfcc_dir = os.path.join(datadir, level, dataset, 'feature')
-test_label_dir = os.path.join(datadir, level, dataset, 'label')
+
+def get_data(datadir, level, train_dataset, dev_dataset, test_dataset, mode):
+    if mode == 'train':
+        train_feature_dirs = [os.path.join(os.path.join(datadir, level, train_dataset), 
+            i, 'feature') for i in os.listdir(os.path.join(datadir, level, train_dataset))]
+
+        train_label_dirs = [os.path.join(os.path.join(datadir, level, train_dataset), 
+            i, 'label') for i in os.listdir(os.path.join(datadir, level, train_dataset))]
+        return train_feature_dirs, train_label_dirs
+
+    if mode == 'dev':
+        dev_feature_dirs = [os.path.join(os.path.join(datadir, level, dev_dataset), 
+            i, 'feature') for i in os.listdir(os.path.join(datadir, level, dev_dataset))]
+
+        dev_label_dirs = [os.path.join(os.path.join(datadir, level, dev_dataset), 
+            i, 'label') for i in os.listdir(os.path.join(datadir, level, dev_dataset))]
+        return dev_feature_dirs, dev_label_dirs
+
+    if mode == 'test':
+        test_feature_dirs = [os.path.join(os.path.join(datadir, level, test_dataset), 
+            i, 'feature') for i in os.listdir(os.path.join(datadir, level, test_dataset))]
+
+        test_label_dirs = [os.path.join(os.path.join(datadir, level, test_dataset), 
+            i, 'label') for i in os.listdir(os.path.join(datadir, level, test_dataset))]
+        return test_feature_dirs, test_label_dirs
+
 logfile = os.path.join(loggingdir, str(datetime.datetime.strftime(datetime.datetime.now(), 
     '%Y-%m-%d %H:%M:%S') + '.txt').replace(' ', '').replace('/', ''))
 
@@ -130,21 +160,16 @@ class Runner(object):
             }
 
     @describe
-    def load_data(self, mode, level):
-        if mode == 'train':
-            return load_batched_data(train_mfcc_dir, train_label_dir, batch_size, mode, level)
-        elif mode == 'test':
-            return load_batched_data(test_mfcc_dir, test_label_dir, batch_size, mode, level)
-        else:
-            raise TypeError('mode should be train or test.')
+    def load_data(self, feature_dir, label_dir, mode, level):
+        return load_batched_data(feature_dir, label_dir, batch_size, mode, level)
 
     def run(self):
         # load data
         args_dict = self._default_configs()
         args = dotdict(args_dict)
-        batchedData, maxTimeSteps, totalN = self.load_data(mode=mode, level=level)
+        feature_dirs, label_dirs = get_data(datadir, level, train_dataset, dev_dataset, test_dataset, mode)
+        batchedData, maxTimeSteps, totalN = self.load_data(feature_dirs[0], label_dirs[1], mode, level)
         model = model_fn(args, maxTimeSteps)
-
         # count the num of params
         num_params = count_params(model, mode='trainable')
         all_num_params = count_params(model, mode='all')
@@ -168,74 +193,79 @@ class Runner(object):
                 start = time.time()
                 if mode == 'train':
                     print('Epoch', epoch + 1, '...')
-                batchErrors = np.zeros(len(batchedData))
-                batchRandIxs = np.random.permutation(len(batchedData))
+                
+                
+                for feature_dir, label_dir in zip(feature_dirs, label_dirs):
+                    batchedData, maxTimeSteps, totalN = self.load_data(feature_dir, label_dir, mode, level)
 
-                for batch, batchOrigI in enumerate(batchRandIxs):
-                    batchInputs, batchTargetSparse, batchSeqLengths = batchedData[batchOrigI]
-                    batchTargetIxs, batchTargetVals, batchTargetShape = batchTargetSparse
-                    feedDict = {model.inputX: batchInputs, model.targetIxs: batchTargetIxs,
-                                model.targetVals: batchTargetVals, model.targetShape: batchTargetShape,
-                                model.seqLengths: batchSeqLengths}
+                    batchErrors = np.zeros(len(batchedData))
+                    batchRandIxs = np.random.permutation(len(batchedData))
 
-                    if level == 'cha':
-                        if mode == 'train':
-                            _, l, pre, y, er = sess.run([model.optimizer, model.loss,
-                                model.predictions, model.targetY, model.errorRate],
-                                feed_dict=feedDict)
+                    for batch, batchOrigI in enumerate(batchRandIxs):
+                        batchInputs, batchTargetSparse, batchSeqLengths = batchedData[batchOrigI]
+                        batchTargetIxs, batchTargetVals, batchTargetShape = batchTargetSparse
+                        feedDict = {model.inputX: batchInputs, model.targetIxs: batchTargetIxs,
+                                    model.targetVals: batchTargetVals, model.targetShape: batchTargetShape,
+                                    model.seqLengths: batchSeqLengths}
 
-                            batchErrors[batch] = er
-                            print('\n{} mode, total:{},batch:{}/{},epoch:{}/{},train loss={:.3f},mean train CER={:.3f}\n'.format(
-                                level, totalN, batch+1, len(batchRandIxs), epoch+1, num_epochs, l, er/batch_size))
+                        if level == 'cha':
+                            if mode == 'train':
+                                _, l, pre, y, er = sess.run([model.optimizer, model.loss,
+                                    model.predictions, model.targetY, model.errorRate],
+                                    feed_dict=feedDict)
 
-                        elif mode == 'test':
-                            l, pre, y, er = sess.run([model.loss, model.predictions, 
-                                model.targetY, model.errorRate], feed_dict=feedDict)
-                            batchErrors[batch] = er
-                            print('\n{} mode, total:{},batch:{}/{},test loss={:.3f},mean test CER={:.3f}\n'.format(
-                                level, totalN, batch+1, len(batchRandIxs), l, er/batch_size))
+                                batchErrors[batch] = er
+                                print('\n{} mode, total:{},batch:{}/{},epoch:{}/{},train loss={:.3f},mean train CER={:.3f}\n'.format(
+                                    level, totalN, batch+1, len(batchRandIxs), epoch+1, num_epochs, l, er/batch_size))
 
-                    elif level=='seq2seq':
-                        raise ValueError('level %s is not supported now'%str(level))
+                            elif mode == 'test':
+                                l, pre, y, er = sess.run([model.loss, model.predictions, 
+                                    model.targetY, model.errorRate], feed_dict=feedDict)
+                                batchErrors[batch] = er
+                                print('\n{} mode, total:{},batch:{}/{},test loss={:.3f},mean test CER={:.3f}\n'.format(
+                                    level, totalN, batch+1, len(batchRandIxs), l, er/batch_size))
+
+                        elif level=='seq2seq':
+                            raise ValueError('level %s is not supported now'%str(level))
 
 
-                    # NOTE:
-                    if er / batch_size == 1.0:
-                        break
+                        # NOTE:
+                        if er / batch_size == 1.0:
+                            break
 
-                    if batch % 30 == 0:
-                        print('Truth:\n' + output_to_sequence(y, type=level))
-                        print('Output:\n' + output_to_sequence(pre, type=level))
+                        if batch % 30 == 0:
+                            print('Truth:\n' + output_to_sequence(y, type=level))
+                            print('Output:\n' + output_to_sequence(pre, type=level))
 
                     
-                    if mode=='train' and ((epoch * len(batchRandIxs) + batch + 1) % 20 == 0 or (
-                           epoch == num_epochs - 1 and batch == len(batchRandIxs) - 1)):
-                        checkpoint_path = os.path.join(savedir, 'model.ckpt')
-                        model.saver.save(sess, checkpoint_path, global_step=epoch)
-                        print('Model has been saved in {}'.format(savedir))
+                        if mode=='train' and ((epoch * len(batchRandIxs) + batch + 1) % 20 == 0 or (
+                            epoch == num_epochs - 1 and batch == len(batchRandIxs) - 1)):
+                            checkpoint_path = os.path.join(savedir, 'model.ckpt')
+                            model.saver.save(sess, checkpoint_path, global_step=epoch)
+                            print('Model has been saved in {}'.format(savedir))
 
-                end = time.time()
-                delta_time = end - start
-                print('Epoch ' + str(epoch + 1) + ' needs time:' + str(delta_time) + ' s')
+                    end = time.time()
+                    delta_time = end - start
+                    print('Epoch ' + str(epoch + 1) + ' needs time:' + str(delta_time) + ' s')
 
-                if mode=='train':
-                    if (epoch + 1) % 1 == 0:
-                        checkpoint_path = os.path.join(savedir, 'model.ckpt')
-                        model.saver.save(sess, checkpoint_path, global_step=epoch)
-                        print('Model has been saved in {}'.format(savedir))
-                    epochER = batchErrors.sum() / totalN
-                    print('Epoch', epoch + 1, 'mean train error rate:', epochER)
-                    logging(model, logfile, epochER, epoch, delta_time, mode='config')
-                    logging(model, logfile, epochER, epoch, delta_time, mode=mode)
+                    if mode=='train':
+                        if (epoch + 1) % 1 == 0:
+                            checkpoint_path = os.path.join(savedir, 'model.ckpt')
+                            model.saver.save(sess, checkpoint_path, global_step=epoch)
+                            print('Model has been saved in {}'.format(savedir))
+                        epochER = batchErrors.sum() / totalN
+                        print('Epoch', epoch + 1, 'mean train error rate:', epochER)
+                        logging(model, logfile, epochER, epoch, delta_time, mode='config')
+                        logging(model, logfile, epochER, epoch, delta_time, mode=mode)
 
-                if mode=='test':
-                    with open(os.path.join(resultdir, level + '_result.txt'), 'a') as result:
-                        result.write(output_to_sequence(y, type=level) + '\n')
-                        result.write(output_to_sequence(pre, type=level) + '\n')
-                        result.write('\n')
-                    epochER = batchErrors.sum() / totalN
-                    print(' test error rate:', epochER)
-                    logging(model, logfile, epochER, mode=mode)
+                    if mode=='test':
+                        with open(os.path.join(resultdir, level + '_result.txt'), 'a') as result:
+                            result.write(output_to_sequence(y, type=level) + '\n')
+                            result.write(output_to_sequence(pre, type=level) + '\n')
+                            result.write('\n')
+                        epochER = batchErrors.sum() / totalN
+                        print(' test error rate:', epochER)
+                        logging(model, logfile, epochER, mode=mode)
 
 
 
