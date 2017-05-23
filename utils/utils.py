88dcedf98cbac52559b9313ade427f3dd04f3290
nemik <nemik@nemik.net>
2017-05-22 20:56:51 -0500

    avoid copy/pasting code, max max-length an optional arg so existing code works, with default value

--- utils/utils.py
+++ utils/utils.py
@@ -239,14 +239,14 @@ def get_edit_distance(hyp_arr, truth_arr, normalize, level):
         dist = session.run(editDist, feed_dict=feedDict)
     return dist
 
-def data_lists_to_batches(inputList, targetList, batchSize, level):
+def data_lists_to_batches(inputList, targetList, batchSize, level, maxlen=0):
     ''' padding the input list to a same dimension, integrate all data into batchInputs
     '''
     assert len(inputList) == len(targetList)
     # dimensions of inputList:batch*39*time_length
 
     nFeatures = inputList[0].shape[0]
-    maxLength = 0
+    maxLength = maxlen
     for inp in inputList:
 	    # find the max time_length
         maxLength = max(maxLength, inp.shape[1])
@@ -277,46 +277,6 @@ def data_lists_to_batches(inputList, targetList, batchSize, level):
         start += batchSize
         end += batchSize
     return (dataBatches, maxLength)
-
-# configurable maxlength to do padding on input here
-def data_lists_to_batches(inputList, targetList, batchSize, level, maxLength):
-    ''' padding the input list to a same dimension, integrate all data into batchInputs
-    '''
-    assert len(inputList) == len(targetList)
-    # dimensions of inputList:batch*39*time_length
-
-    nFeatures = inputList[0].shape[0]
-    for inp in inputList:
-	    # find the max time_length
-        maxLength = max(maxLength, inp.shape[1])
-
-    # randIxs is the shuffled index from range(0,len(inputList))
-    randIxs = np.random.permutation(len(inputList))
-    start, end = (0, batchSize)
-    dataBatches = []
-
-    while end <= len(inputList):
-	    # batchSeqLengths store the time-length of each sample in a mini-batch
-        batchSeqLengths = np.zeros(batchSize)
-
-  	    # randIxs is the shuffled index of input list
-        for batchI, origI in enumerate(randIxs[start:end]):
-            batchSeqLengths[batchI] = inputList[origI].shape[-1]
-
-        batchInputs = np.zeros((maxLength, batchSize, nFeatures))
-        batchTargetList = []
-        for batchI, origI in enumerate(randIxs[start:end]):
-	        # padSecs is the length of padding
-            padSecs = maxLength - inputList[origI].shape[1]
-	        # numpy.pad pad the inputList[origI] with zeos at the tail
-            batchInputs[:,batchI,:] = np.pad(inputList[origI].T, ((0,padSecs),(0,0)), 'constant', constant_values=0)
-	        # target label
-            batchTargetList.append(targetList[origI])
-        dataBatches.append((batchInputs, list_to_sparse_tensor(batchTargetList, level), batchSeqLengths))
-        start += batchSize
-        end += batchSize
-    return (dataBatches, maxLength)
-
 
 def load_batched_data(mfccPath, labelPath, batchSize, mode, level):
     '''returns 3-element tuple: batched data (list), maxTimeLength (int), and
