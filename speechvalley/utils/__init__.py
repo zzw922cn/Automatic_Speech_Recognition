57f32bd6553d3d586b179e6aa247432044473653
zzw922cn <zzw922cn@gmail.com>
2017-12-24 10:21:07 +0800

    add visualization.py

--- speechvalley/utils/__init__.py
+++ speechvalley/utils/__init__.py
@@ -1,9 +1,12 @@
-#-*- coding:utf-8 -*-
-#!/usr/bin/env python3
-""" Speech Valley
-@author: zzw922cn
-@date: 2017-12-02
-"""
+# encoding: utf-8
+# ******************************************************
+# Author       : zzw922cn
+# Last modified: 2017-12-09 11:00
+# Email        : zzw922cn@gmail.com
+# Filename     : __init__.py
+# Description  : Utils function for Automatic Speech Recognition
+# ******************************************************
+
 from speechvalley.utils.calcPER import calc_PER
 from speechvalley.utils.functionDictUtils import activation_functions_dict, optimizer_functions_dict 
 from speechvalley.utils.lnRNNCell import BasicRNNCell as lnBasicRNNCell
@@ -11,3 +14,4 @@ from speechvalley.utils.lnRNNCell import GRUCell as lnGRUCell
 from speechvalley.utils.lnRNNCell import BasicLSTMCell as lnBasicLSTMCell
 from speechvalley.utils.taskUtils import check_path_exists, get_num_classes, dotdict 
 from speechvalley.utils.utils import setAttrs, getAttrs, describe, dropout, batch_norm, data_lists_to_batches, load_batched_data, list_dirs, get_edit_distance, output_to_sequence, target2phoneme, count_params, logging, list_to_sparse_tensor 
+from speechvalley.utils.visualization import plotWaveform
---
+++ speechvalley/utils/visualization.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+# ******************************************************
+# Author       : zzw922cn
+# Last modified: 2017-12-23 11:00
+# Email        : zzw922cn@gmail.com
+# Filename     : visualization.py
+# Description  : Some common functions for visualization
+# ******************************************************
+
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+import sys
+
+def plotWaveform(audio):
+    """
+    Plotting the waveform of a wav file
+    """
+    spf = wave.open(audio,'r')
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, 'Int16')
+    fs = spf.getframerate()
+    if spf.getnchannels() == 2:
+        print 'Just mono files'
+        sys.exit(0)
+    Time=np.linspace(0, len(signal)/fs, num=len(signal))
+    plt.figure(1)
+    plt.title('Waveform of an audio')
+    plt.plot(Time,signal)
+    plt.show()
